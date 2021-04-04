8de585f5930a1db1335060f0987f226e5eb57615
y4kw <y4kw@math.sci.hokudai.ac.jp>
2021-04-04 18:38:32 +0900

    Initial commit

---
+++ app/src/main/java/com/example/pp/MainActivity.java
@@ -0,0 +1,113 @@
+package com.example.pp;
+
+import android.content.Context;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    // JavaScriptInterface
+    // â»JavaScriptãããã®ã¯ã©ã¹ã®ã¡ã½ãããå¼ã³åºã
+    private class JavaScriptInterface {
+
+        @JavascriptInterface
+        public void MyMethod(String str) {
+            // ã­ã°ã«åºåãã
+            Log.i("MyTAG",str);
+        }
+    }
+
+    // ãªã½ã¼ã¹ãããã¡ã¤ã«ãçæãã(/data/data/ããã±ã¼ã¸å/files/)
+    public boolean setRawResources(Context context , int resourcesID, String fileName){
+        boolean result = false;
+
+        // ãªã½ã¼ã¹ã®èª­ã¿è¾¼ã¿
+        InputStream is =  context.getResources().openRawResource(resourcesID);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        byte [] buffer = new byte[1024];
+        try{
+            // 1024ãã¤ãæ¯ããã¡ã¤ã«ãèª­ã¿è¾¼ã
+            while(true) {
+                int len = is.read(buffer);
+                if(len < 0)  break;
+                baos.write(buffer, 0, len);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+            return result;
+        }
+
+        // ãã¡ã¤ã«ã®çæ
+        File file = new File(context.getFilesDir() + "/" + fileName);
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(file);
+            fos.write(baos.toByteArray());
+            result = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if(fos != null){
+                try{
+                    fos.close();
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
+        return result;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final WebView webView = findViewById(R.id.webView);
+
+        // ãªã½ã¼ã¹ãããã¡ã¤ã«ãçæãã(/data/data/ããã±ã¼ã¸å/files/ã«ä½æ)
+        setRawResources(this, R.raw.index, "index.html");
+
+        // ã­ã£ãã·ã¥ã¯ãªã¢
+        // â»éçºæã®ã¿æå¹ã«ãã
+        webView.clearCache(true);
+
+        // JavaScriptãæå¹ã«ãã
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        // WebChromeClientãè¨­å®ãã
+        // â»ã³ã¬ãè¨­å®ããªãã¨JSã®alertã¯è¡¨ç¤ºãããªã
+        webView.setWebChromeClient(new WebChromeClient());
+
+        // JavaScriptInterfaceã®åæè¨­å®
+        webView.addJavascriptInterface(new JavaScriptInterface(), "android");
+
+        // ãã¡ã¤ã«ãèª­ã¿è¾¼ã
+        webView.loadUrl("file:///" + MainActivity.this.getFilesDir() + "/index.html");
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                WebView webView = findViewById(R.id.webView);
+
+                // JavaããJavaScriptãå®è¡ãã
+                // â»ãjavascript:ãã®å¾ã«JavaScriptãè¨è¿°ãã
+                webView.loadUrl("javascript:" +
+                        "MsgBox('showme');");
+
+            }
+        });
+
+    }
+}
